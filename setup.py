ff986c0642f1cf4babb2beb763d707b80fcbddef
orinxam <mpn26@cam.ac.uk>
2021-06-03 17:04:43 +0100

    worked on win binaries

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 import platform
-
+from glob import glob
 
 # Determine the correct platform for binaries
 system = platform.system()
@@ -10,18 +10,19 @@ if system == 'Linux':
     OPTIM = 'bin/linux/OPTIM'
     PATHSAMPLE = 'bin/linux/PATHSAMPLE'
     disconnectionDPS = 'bin/linux/disconnectionDPS'
+    os='linux'
 if system == 'Windows':
     GMIN = 'bin/win/GMIN.exe'
     OPTIM = 'bin/win/OPTIM.exe'
     PATHSAMPLE = 'bin/win/PATHSAMPLE.exe'
     disconnectionDPS = 'bin/win/disconnectionDPS.exe'
+    os='win'
 if system == 'Darwin':
     GMIN = 'bin/mac/GMIN'
     OPTIM = 'bin/mac/OPTIM'
     PATHSAMPLE = 'bin/mac/PATHSAMPLE'
     disconnectionDPS = 'bin/mac/disconnectionDPS'
-
-print(gmin)
+    os='mac'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -46,7 +47,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    data_files = [('bin', [gmin,optim,pathsample,disconnectionDPS])],
+    data_files = [('bin',glob(f'{os}/*'))],
 )
 
 
